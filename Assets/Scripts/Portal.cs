6ef765b3368d89dc9f4e34a8b44b177298fcb06b
HarrisAlex <alexharris7639@gmail.com>
2022-03-28 21:02:34 -0400

    Portals rendering finished

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -15,4 +15,55 @@ public class Portal : MonoBehaviour
     private Material viewThroughMaterial;
 
     private Camera mainCamera;
+
+    private Vector4 vectorPlane;
+
+    void Start()
+    {
+        viewThroughRenderTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.DefaultHDR);
+        viewThroughRenderTexture.Create();
+
+        viewThroughMaterial = viewThroughRenderer.material;
+        viewThroughMaterial.mainTexture = viewThroughRenderTexture;
+
+        portalCamera.targetTexture = viewThroughRenderTexture;
+
+        mainCamera = Camera.main;
+
+        var plane = new Plane(normalVisible.forward, transform.position);
+        vectorPlane = new Vector4(plane.normal.x, plane.normal.y, plane.normal.z, plane.distance);
+    }
+
+    void LateUpdate()
+    {
+        var virtualPosition = TransformPositionBetweenPortals(this, targetPortal, mainCamera.transform.position);
+        var virtualRotation = TransformRotationBetweenPortals(this, targetPortal, mainCamera.transform.rotation);
+
+        portalCamera.transform.SetPositionAndRotation(virtualPosition, virtualRotation);
+
+        var clipThroughSpace = Matrix4x4.Transpose(Matrix4x4.Inverse(portalCamera.worldToCameraMatrix)) * targetPortal.vectorPlane;
+
+        var obliqueProjectionMatrix = mainCamera.CalculateObliqueMatrix(clipThroughSpace);
+        portalCamera.projectionMatrix = obliqueProjectionMatrix;
+
+        portalCamera.Render();
+    }
+
+    void OnDestroy()
+    {
+        viewThroughRenderTexture.Release();
+
+        Destroy(viewThroughMaterial);
+        Destroy(viewThroughRenderTexture);
+    }
+
+    public static Vector3 TransformPositionBetweenPortals(Portal sender, Portal target, Vector3 position)
+    {
+        return target.normalInvisible.TransformPoint(sender.normalVisible.InverseTransformPoint(position));
+    }
+
+    public static Quaternion TransformRotationBetweenPortals(Portal sender, Portal target, Quaternion rotation)
+    {
+        return target.normalInvisible.rotation * Quaternion.Inverse(sender.normalVisible.rotation) * rotation;
+    }
 }
